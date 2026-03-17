af3dd72346b0f803f485b8825c6a43d580d81c5d
Tuomas Alanen <tuomas@alanen.dev>
2026-03-17 22:45:00 +0200

    Debug lambda function uri

--- infra/domain.tf
+++ infra/domain.tf
@@ -37,10 +37,18 @@ resource "cloudflare_record" "sisubot" {
   proxied = false
 }
 
-resource "cloudflare_record" "sisubot_api" {
-  zone_id = data.cloudflare_zone.alanendev.id
-  name    = "api.sis-u-bot"
-  value   = aws_lambda_function_url.api.function_url
-  type    = "CNAME"
-  proxied = false
+# resource "cloudflare_record" "sisubot_api" {
+#   zone_id = data.cloudflare_zone.alanendev.id
+#   name    = "api.sis-u-bot"
+#   value   = aws_lambda_function_url.api.function_url
+#   type    = "CNAME"
+#   proxied = false
+# }
+
+output "cf_uri" {
+  value = aws_cloudfront_distribution.app.domain_name
+}
+
+output "lambda_uri" {
+  value = aws_lambda_function_url.api.function_url
 }
