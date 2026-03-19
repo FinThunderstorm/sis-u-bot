02850fd1e9b1e783107612f03b8198c5aadf21af
Tuomas Alanen <tuomas@alanen.dev>
2026-03-19 20:02:30 +0200

    Convert API to answer something more relevant

--- api/src/sisubot.clj
+++ api/src/sisubot.clj
@@ -4,9 +4,20 @@
   (:require [clojure.pprint :refer [pprint]]))
 
 (defn identify-question [question]
-  (if (re-find #"courses" question) "courses" (
-        if (re-find #"where" question) "where" (
-           if (re-find #"evaluated" question) "evaluated" "unknown"
+  (if (re-find #"courses" question) """Based on your previous completed courses, I suggest you take following course:
+           |Course                                    |Course code|Credits|
+           |------------------------------------------|-----------|-------|
+           |Digital Transformation Management         |YY00BW98   |5      |
+  """ (
+        if (re-find #"where" question) "You can find your student number from `My profile > Personal information`" (
+           if (re-find #"evaluated" question) """
+           You have got following evaluations in last 30 days:
+           |Course                                    |Course code|Credits|Grade|
+           |------------------------------------------|-----------|-------|-----|
+           |Software Engineering Models and Modeling  |CT60A5103  |6      |5    |
+           |Foundations of Software Product Management|CT70A6201  |6      |4    |
+           |Requirements Engineering                  |CT70A2000  |6      |5    |
+           """ "unknown"
              ))
     )
   )
