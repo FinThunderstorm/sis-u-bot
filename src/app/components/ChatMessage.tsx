51d6c9e557bb794ee0ea3d73c3be4cbd3afbb7bd
Tuomas Alanen <tuomas@alanen.dev>
2026-03-22 20:36:44 +0200

    Optimize gaps

--- src/app/components/ChatMessage.tsx
+++ src/app/components/ChatMessage.tsx
@@ -6,9 +6,9 @@ import {Message} from "@/app/contexts/ChatContext";
 
 export const ChatMessage = ({message: m, loading}: { message: Message, loading?: boolean }) => {
     return <section
-        className={`${m.actor === "user" ? "self-end bg-cyan-100" : "self-start bg-gray-100"} p-4 rounded-lg max-w-(--message-width) overflow-y-auto`}
+        className={`${m.actor === "user" ? "self-end bg-cyan-100 ps-4 pe-2" : "self-start bg-gray-100 ps-2 pe-4"} py-4 rounded-lg max-w-(--message-width) overflow-y-auto`}
     >
-        <div className="flex flex-row gap-8 items-center">
+        <div className="flex flex-row gap-3 items-center">
             {m.actor === "bot" &&
                 <AcademicCapIcon
                     className="h-[40px] w-[40px] bg-white fill-black shrink-0 p-1.5 rounded-full self-start"/>}
@@ -18,7 +18,7 @@ export const ChatMessage = ({message: m, loading}: { message: Message, loading?:
                     <Markdown remarkPlugins={[remarkGfm]} components={{
                         table(props) {
                             const {node, children, ...rest} = props
-                            return <table className="table-auto border" {...rest}>{children}</table>
+                            return <table className="table-auto border me-2" {...rest}>{children}</table>
                         },
                         th(props) {
                             const {node, children, ...rest} = props
