9b24194e1a9b3c693e6fa383304bdf80bbaab2e5
Tuomas Alanen <tuomas@alanen.dev>
2026-03-22 20:30:28 +0200

    Fix response overflow issue

--- src/app/components/ChatMessage.tsx
+++ src/app/components/ChatMessage.tsx
@@ -6,11 +6,12 @@ import {Message} from "@/app/contexts/ChatContext";
 
 export const ChatMessage = ({message: m, loading}: { message: Message, loading?: boolean }) => {
     return <section
-        className={`${m.actor === "user" ? "self-end bg-cyan-100" : "self-start bg-gray-100"} p-4 rounded-lg`}
+        className={`${m.actor === "user" ? "self-end bg-cyan-100" : "self-start bg-gray-100"} p-4 rounded-lg max-w-(--message-width) overflow-y-auto`}
     >
         <div className="flex flex-row gap-8 items-center">
             {m.actor === "bot" &&
-                <AcademicCapIcon className="h-12 bg-white p-2 rounded-full self-start"/>}
+                <AcademicCapIcon
+                    className="h-[40px] w-[40px] bg-white fill-black shrink-0 p-1.5 rounded-full self-start"/>}
             {loading && <Loading/>}
             {m.actor === "user" ? <span>{m.message}</span> :
                 <div className="flex flex-col gap-4">
@@ -29,7 +30,7 @@ export const ChatMessage = ({message: m, loading}: { message: Message, loading?:
                         }
                     }}>{m.message}</Markdown></div>}
             {m.actor === "user" &&
-                <UserIcon className="h-12 bg-white p-2 rounded-full self-start"/>}
+                <UserIcon className="h-[40px] w-[40px] bg-white fill-black shrink-0 p-1.5 rounded-full self-end"/>}
         </div>
     </section>
 }
